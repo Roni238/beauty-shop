d319ccb06ea21bd6548bbad8dcb0608f67b4a688
roni238000000 <ak4932328@gmail.com>
2026-03-01 22:19:45 +0300

    first commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <LayoutsHeader />
+    <main class="main">
+      <slot /> 
+    </main>
+    <LazyLayoutsFooter/>
+  </div>
+</template>
+
+<style lang="scss" soped>
+.main{
+  margin-block: 120px 80px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/scss/_variables.scss" as *;
+            @use "@/assets/scss/_mixins.scss" as *;
+            @use "@/assets/scss/main.scss" as *;
+          `
+        }
+      }
+    }
+  },
+  modules: ["@nuxt/image", "nuxt-studio", "@nuxt/content"]
+})
