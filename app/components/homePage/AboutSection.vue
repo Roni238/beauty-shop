fd045bb33dde6360eb0b693280d73e59b4be275f
roni238000000 <ak4932328@gmail.com>
2026-03-14 02:06:29 +0300

    fix about section

--- app/components/homePage/AboutSection.vue
+++ app/components/homePage/AboutSection.vue
@@ -4,8 +4,12 @@
 <template>
   <section class="about">
     <div class="about__container">
+      <img src="/about-img.png" alt="Payot heritage" class="about__image" />
+
       <div class="about__content">
-        <h2 class="about__heading">The Payot Philosophy</h2>
+        <h2 class="about__heading">
+          <span class="about__heading-sub">Our Philosophy</span>
+          The Payot Philosophy</h2>
         <p class="about__text">
           For over a century, Payot has been at the forefront of French haute cosmétique, 
           blending scientific innovation with natural ingredients to create exceptional 
@@ -16,68 +20,52 @@
           Explore our blog for expert advice, product insights, and the latest beauty 
           trends from our team of skincare professionals.
         </p>
+        <a class="about__link" href="">Learn more about Nadia Payot &nbsp <IconsArrowCTA /></a>
       </div>
-      
-      <img src="/Frame-7586-2.png" alt="Payot heritage" class="about__image" />
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
   .about {
-    padding-top: 40px;
+    padding-block: 40px;
+    background: $pink-gr;
 
     @include tablet {
-      padding-top: 80px;
+      padding-block: 80px;
     }
 
     @include laptop {
-      padding-top: 120px;
+      padding-block: 120px;
     }
 
     &__container {
-      display: flex;
-      flex-direction: column-reverse;
-
       @include container;
       @include tablet {
         display: grid;
         grid-template-columns: 1fr 1fr;
         align-items: center;
         gap: 40px;
-        
-        .about__content {
-          order: 2;
-        }
-        
-        .about__image {
-          order: 1;
-        }
       }
     }
 
     &__content {
       margin: auto;
-      padding-top: 40px;
-
-      @include tablet {
-        padding-top: 0px;
-      }
+      display: grid;
+      gap: 24px;
     }
 
     &__heading {
-      margin-bottom: 40px;
       color: $black-color;
       position: relative;
 
-      &::after {
-        content: '';
-        position: absolute;
-        left: 0;
-        bottom: -20px;
-        width: 80px;
-        height: 4px;
-        background-color: $pink-color;
+      &-sub {
+        display: block;
+        font-size: clamp(1.5rem, 3vw, 1.7rem);
+        font-weight: 700;
+        text-transform: uppercase;
+        color: $pink-color;
+        margin-bottom: 12px;
       }
     }
 
@@ -85,27 +73,21 @@
       font-size: 18px;
       line-height: 1.6;
       color: $gray-3-color;
-      margin-bottom: 24px;
 
       @include tablet {
         font-size: 20px;
       }
     }
 
+    &__link{
+      text-decoration: none;
+      color: $pink-color;
+      font-size: clamp(1.2rem, 3vw, 1.5rem);
+    }
+
     &__image {
-      aspect-ratio: 1 / 1;
       width: 100%;
-      object-fit: cover;
-      box-shadow: 0 20px 50px rgba(0, 0, 0, 0.08);
-
-      @include tablet {
-        height: 400px;
-        width: 100%;
-      }
-
-      @include laptop {
-        height: 600px;
-      }
+      object-fit: contain;
     }
   }
 </style>
