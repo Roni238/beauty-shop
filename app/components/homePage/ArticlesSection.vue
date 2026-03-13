40312e54b242acb6a5d84b81f0c90a79954b9afc
roni238000000 <ak4932328@gmail.com>
2026-03-14 02:22:30 +0300

    update articles section

--- app/components/homePage/ArticlesSection.vue
+++ app/components/homePage/ArticlesSection.vue
@@ -2,13 +2,11 @@
 import { usePagination } from '@/composables/usePagination';
 
 const { data: articles } = await useAsyncData('articles', () => 
-  queryCollection('articles').select('id', 'title', 'price', 'image', 'createdAt').order('createdAt', 'DESC').all()
+  queryCollection('articles').select('id', 'title', 'preview', 'image', 'createdAt').order('createdAt', 'DESC').all()
 )
 
 const itemsPerPage = ref(4)
-onMounted(() => {
-  itemsPerPage.value = window.innerWidth >= 1280 ? 4 : window.innerWidth >= 640 ? 2 : 1
-})
+itemsPerPage.value = window.innerWidth >= 1280 ? 8 : window.innerWidth >= 768 ? 4 : window.innerWidth >= 640 ? 2 : 1
 
 const { 
   currentPage, 
