f8f7a7a7a6b56d6efc1cf5c567875d45eb139717
roni238000000 <ak4932328@gmail.com>
2026-03-14 05:59:54 +0300

    update articles sectionwwd

--- content.config.ts
+++ content.config.ts
@@ -9,11 +9,19 @@ export default defineContentConfig({
         exclude: ['**/*.md', '**/*.md/**', '**/*.json.md']
       },
       schema: z.object({
-        title: z.string(),
-        description: z.string().min(10, 'Описание должно быть не менее 10 символов'),
-        createdAt: z.string().datetime(),
-        price: z.number().optional(),
-        image: z.string().url('Должен быть валидный URL').optional(),
-      })
+  title: z.string().optional(),
+  description: z.string().optional(),
+  // Убираем жесткую проверку datetime и url на время теста
+  createdAt: z.string().optional(), 
+  price: z.number().optional(),
+  image: z.string().optional(),
+})
+      // schema: z.object({
+      //   title: z.string(),
+      //   description: z.string().min(10, 'Описание должно быть не менее 10 символов'),
+      //   createdAt: z.string().datetime(),
+      //   price: z.number().optional(),
+      //   image: z.string().url('Должен быть валидный URL').optional(),
+      // })
     })}
 })
